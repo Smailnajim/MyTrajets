9da1c678d4548443e40b9ce9a9e5a1efd90d02eb
smailnajim <smailnajim2003@gmail.com>
2025-12-09 11:04:17 +0100

    error message

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -5,9 +5,9 @@ dotenv.config();
 const connectionDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`conection with db on port: ${conn.connection.host}`);
+        console.log("conection with db");
     } catch (error) {
-        console.log(`conection with db is filed`);
+        console.log(`conection with db is filed: ${error.message}`);
         process.exit(1);
     }
 }
