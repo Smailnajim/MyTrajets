e37e90bc6930a5c706fcbfadeac97b2c561fc3be
smailnajim <smailnajim2003@gmail.com>
2025-12-10 19:49:47 +0100

    MYT-25 #done CRUD modèle "Role"

--- Backend/src/repositories/Roles.js
+++ Backend/src/repositories/Roles.js
@@ -1,12 +1,37 @@
 import Role from "../models/Role.js";
 
+// CREATE
 const createRole = async (role) => {
     const newRole = new Role(role);
     return await newRole.save();
 }
+
+// READ
 const findOne = async (role) => {
     return await Role.findOne(role);
 }
+
+const findOneById = async (id) => {
+    return await Role.findById(id);
+}
+
+const findByName = async (name) => {
+    return await Role.findOne({ name });
+}
+
+const findAll = async () => {
+    return await Role.find();
+}
+
+const findAllWithFilters = async (filters = {}) => {
+    return await Role.find(filters);
+}
+
+// UPDATE
+const updateRole = async (id, updateData) => {
+    return await Role.findByIdAndUpdate(id, updateData, { new: true });
+}
+
 const findOneAndUpdatePermissions = async (role, permissions) => {
     return await Role.findOneAndUpdate(
         { name: role },
@@ -14,12 +39,25 @@ const findOneAndUpdatePermissions = async (role, permissions) => {
         { new: true }
     );
 }
-const findOneById = async (id) => {
-    return await Role.findById(id);
+
+// DELETE
+const deleteRole = async (id) => {
+    return await Role.findByIdAndDelete(id);
 }
+
+const deleteByName = async (name) => {
+    return await Role.findOneAndDelete({ name });
+}
+
 export default {
     createRole,
     findOne,
+    findOneById,
+    findByName,
+    findAll,
+    findAllWithFilters,
+    updateRole,
     findOneAndUpdatePermissions,
-    findOneById
+    deleteRole,
+    deleteByName
 };
