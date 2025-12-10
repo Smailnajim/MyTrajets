f08944f92a53d4bd31ad2d98f6871af20c4fbc5c
smailnajim <smailnajim2003@gmail.com>
2025-12-10 13:10:01 +0100

    build a structure to update roles in db an check Authorization

---
+++ Backend/src/enums/permitions_roles.js
@@ -0,0 +1,7 @@
+import permitions from "./permitions";
+
+const permitions_roles = {
+    admin: [permitions.accept_user],
+    user: []
+};
+export default permitions_roles;
--- Backend/src/routes/api.js
+++ Backend/src/routes/api.js
@@ -2,6 +2,7 @@ import express from "express";
 import { register, login, refreshToken, acceptUser } from "../controllers/AuthController.js";
 import { registerValidation, loginValidation, validate } from "../validators/authValidator.js";
 import isAuth from "../middlewares/isAuth.js";
+import permitions from "../enums/permitions.js";
 const router = express.Router();
 
 
@@ -13,6 +14,6 @@ router.get('/', (req, res) => {
 router.post('/users/register', registerValidation, validate, register);
 router.post('/users/login', loginValidation, validate, login);
 router.post('/users/refresh-token', refreshToken);
-router.patch('/users/:id/accept', isAuth, acceptUser);
+router.patch('/users/:id/accept', isAuth, iCan(permitions.accept_user), acceptUser);
 
 export default router;
