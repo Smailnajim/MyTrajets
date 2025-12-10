befa75bc6ae9571f629a444f7a84e998702b9257
smailnajim <smailnajim2003@gmail.com>
2025-12-10 16:03:42 +0100

    CRUD modèle "user"

--- Backend/src/repositories/Users.js
+++ Backend/src/repositories/Users.js
@@ -1,6 +1,6 @@
 import User from "../models/User.js";
 
-
+// CREATE
 const createUser = async ({ roleId, firstName, lastName, email, password }) => {
     const user = new User({
         roleId,
@@ -9,25 +9,57 @@ const createUser = async ({ roleId, firstName, lastName, email, password }) => {
         email,
         password,
     });
-
     return await user.save();
 }
 
+// READ
 const findOneByEmail = async ({ email }) => {
     return await User.findOne({ email });
 }
 
-const findOneById = async ({ id }) => {
+const findOneById = async (id) => {
     return await User.findById(id);
 }
 
+const findAll = async () => {
+    return await User.find().select('-password');
+}
+
+const findAllWithFilters = async (filters = {}) => {
+    return await User.find(filters).select('-password').populate('roleId', 'name');
+}
+
+// UPDATE
 const updateUserStatus = async ({ id, etat }) => {
     return await User.findByIdAndUpdate(id, { etat }, { new: true }).select('-password');
+}
+
+const updateUser = async (id, updateData) => {
+    return await User.findByIdAndUpdate(id, updateData, { new: true }).select('-password');
+}
+
+const updatePassword = async (id, password) => {
+    const user = await User.findById(id);
+    if (user) {
+        user.password = password;
+        await user.save();
+    }
+    return user;
+}
+
+// DELETE
+const deleteUser = async (id) => {
+    return await User.findByIdAndDelete(id);
 }
 
 export default {
     createUser,
     findOneByEmail,
     findOneById,
-    updateUserStatus
+    findAll,
+    findAllWithFilters,
+    updateUserStatus,
+    updateUser,
+    updatePassword,
+    deleteUser
 };
