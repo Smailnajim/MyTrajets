e23dffceb45e68b5b138f7ab8da9883075b1daf4
smailnajim <smailnajim2003@gmail.com>
2025-12-10 09:00:03 +0100

    gestion role not exist

--- Backend/src/services/AuthService.js
+++ Backend/src/services/AuthService.js
@@ -12,6 +12,10 @@ export const registerService = async ({ firstName, lastName, email, password, ro
         throw createError("Email already registered", 409);
     }
     const chauffeurRole = await Role.findOne({name: "chauffeur"});
+    if (chauffeurRole) {
+        // throw createError("there is no role has name chauffeur", 409);
+        //must add gestion to create the role if not exist; but not now!
+    }
     const user = createUser({ roleId: chauffeurRole._id, firstName, lastName, email, password})
     const accessToken = generateAccessToken(user);
     const refreshToken = generateRefreshToken(user);
