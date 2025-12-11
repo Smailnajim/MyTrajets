de88b8d2f86b98a32272d80cf6bc50c0b708e9ae
smailnajim <smailnajim2003@gmail.com>
2025-12-11 10:43:27 +0100

    use repository to filter

--- Backend/src/services/TrajetService.js
+++ Backend/src/services/TrajetService.js
@@ -1,10 +1,16 @@
-
+import Trajets from "../repositories/Trajets.js";
 
 /**
- * get all trajets using a filter 
+ * Get all trajets using a filter 
  * admin: can filter from all trajets
  * chauffeur: can filter from just his trajets
- * @returns {Promise<>}
+ * @param {Object} filters
+ * @returns {Promise<Array>}
  */
-const getAllTrajets = async (filters) => {
-}
+const getAllTrajets = async (filters = {}) => {
+    return await Trajets.findAllWithFilters(filters);
+};
+
+export default {
+    getAllTrajets
+};
