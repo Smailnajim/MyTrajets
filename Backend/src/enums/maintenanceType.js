4544b68b6f5924757b40353a50e5a9ec390c948a
smailnajim <smailnajim2003@gmail.com>
2025-12-13 17:22:02 +0100

    maintenanceType

---
+++ Backend/src/enums/maintenanceType.js
@@ -0,0 +1,9 @@
+export default [
+    'vidange',          // Oil change
+    'pneus',            // Tire replacement/check
+    'freins',           // Brakes check/replacement
+    'mecanique',        // General mechanical repair
+    'electrique',       // Electrical repair
+    'visite_technique', // Technical inspection
+    'autre'             // Other
+];
